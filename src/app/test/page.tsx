e48a31151494d88474dd46b81048f3110541e8bf
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2024-03-02 22:18:31 +0100

    fix: infinity loop deleted

--- src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export default async function Page() {
-  await sleep(100000);
-  return <div>test</div>;
-}
